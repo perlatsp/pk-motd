da4c29c390e3c21472ff011a66533864b7ba1f81
Perlat Kociaj <perlatkociaj@gmail.com>
2022-12-17 11:33:35 +0000

    change: remove select

--- install.sh
+++ install.sh
@@ -2,21 +2,12 @@
 #@author Perlat
 # inspired by various scripts on Github and the Wild Internet
 clear
-install_pk_motd(){
-    echo "Downloading script from github..."
-    wget -q -O custom-welcome-message.txt https://raw.githubusercontent.com/perlatsp/pk-motd/main/custom-welcome-message.txt
-    echo "Moving custom-welcome-message to /etc/update-motd.d/custom-welcome-message ...";
-    mv custom-welcome-message.sh /etc/update-motd.d/custom-welcome-message
-    echo "Setting permissions..."
-    sudo chmod -x etc/update-motd.d/*
-    sudo chmod 755 /etc/update-motd.d/custom-welcome-message
-    echo "Finished. Logout and login again to see the new welcome message!"
-}
 
-echo "Do you want to install Custom LEMP Welcome Message?"
-select answer in "Yes" "No"; do
-    case $answer in
-        Yes ) install_pk_motd; break;;
-        No ) echo "OK 🙁, bye now"; exit;;
-    esac
-done
+echo "Downloading script from github..."
+wget -q -O custom-welcome-message.txt https://raw.githubusercontent.com/perlatsp/pk-motd/main/custom-welcome-message.txt
+echo "Moving custom-welcome-message to /etc/update-motd.d/custom-welcome-message ...";
+mv custom-welcome-message.sh /etc/update-motd.d/custom-welcome-message
+echo "Setting permissions..."
+sudo chmod -x etc/update-motd.d/*
+sudo chmod 755 /etc/update-motd.d/custom-welcome-message
+echo "Finished. Logout and login again to see the new welcome message!"
